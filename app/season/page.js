243f625597db906c93fe7751d691fa8992b387f5
Guitar <jilamikalalae@gmail.com>
2024-09-15 18:14:04 +0700

    delete comment

--- app/season/page.js
+++ app/season/page.js
@@ -1,15 +1,13 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+
 export default function Season() {
   const searchParams = useSearchParams();
   const season = searchParams.get("season");
   return (
-    // <Suspense>
     <div>
       <p>highlight season {season ? season : "yo"}</p>
     </div>
-    // </Suspense>
   );
 }
